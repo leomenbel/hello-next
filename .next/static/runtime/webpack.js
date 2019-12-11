abd388034e29049fbc18e4d406dbce244f9bdd52
Leo <leomenbel93@gmail.com>
2019-12-11 12:01:09 -0700

    added fetching data for pages

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0edb9acd220f382478d1";
+/******/ 	var hotCurrentHash = "5f1f1adda41ab454f3a2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
