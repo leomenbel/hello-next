4ca8f75c0f1190444b76f3e44cf0802133226f3e
Leo <leomenbel93@gmail.com>
2019-12-11 12:51:24 -0700

    added styling

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5f1f1adda41ab454f3a2";
+/******/ 	var hotCurrentHash = "28d8a77cf1d2ce0362bb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
